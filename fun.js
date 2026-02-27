da9f79dbca796df575c6dbd713838ada23982350
aLucard <119305265+a-Lucar-d@users.noreply.github.com>
2026-02-27 23:02:30 +0530

    Update fun.js

--- fun.js
+++ fun.js
@@ -1,14 +1,1 @@
-// This runs immediately when the script loads
-fetch("https://7iqxezykafauh5rpv2egj521eskj89wy.oastify.com")
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.json(); // change to .text() if needed
-    })
-    .then(data => {
-        console.log("Response Data:", data);
-    })
-    .catch(error => {
-        console.error("Error:", error);
-    });
+  document.getElementById("title").innerText = "U r Hacked";
