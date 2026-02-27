f94e9fe6ca928523cbc7be78e666e2556c31d26f
aLucard <119305265+a-Lucar-d@users.noreply.github.com>
2026-02-27 22:51:23 +0530

    Update fun.js

--- fun.js
+++ fun.js
@@ -1,1 +1,9 @@
-print()
+(async () => {
+    try {
+        const response = await fetch("https://7iqxezykafauh5rpv2egj521eskj89wy.oastify.com");
+        const data = await response.json();
+        console.log(data);
+    } catch (err) {
+        console.error(err);
+    }
+})();
